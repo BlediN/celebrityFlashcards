3265588edffe5b0fe0183e54e84d894c134aa77f
Bledar N <bledar.n@outlook.com>
2026-01-30 06:21:41 -0500

    tentative to deploy

--- celebrity-flashcards-app/src/main.jsx
+++ celebrity-flashcards-app/src/main.jsx
@@ -4,7 +4,13 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode basename="/celebrityFlashcards/">
     <App />
   </StrictMode>,
 )
+
+// createRoot(document.getElementById('root')).render(
+//   <BrowserRouter basename="/celebrityFlashcards/">
+//     <App />
+//   </BrowserRouter>,
+// )
