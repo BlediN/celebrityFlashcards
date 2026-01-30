e5e09dd5a7d9fbd7b863d5b9edb7a3493a4ba83d
Bledar N <bledar.n@outlook.com>
2026-01-30 06:45:08 -0500

    ready to deploy

--- celebrity-flashcards-app/vite.config.js
+++ celebrity-flashcards-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/celebrityFlashcards/',   // MUST match repo name exactly
+  base: '/celebrity-flashcards-app/',   // MUST match repo name exactly
 })
