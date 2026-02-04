1d4ea74b0333aba15815a6788e1517db17770efa
Bledar N <bledar.n@outlook.com>
2026-02-03 22:43:13 -0500

    fix bug

--- celebrity-flashcards-app/vite.config.js
+++ celebrity-flashcards-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/celebrity-flashcards-app/',   // MUST match repo name exactly
+  base: '/celebrityFlashcards/',   // MUST match repo name exactly
 })
