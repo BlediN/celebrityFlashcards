e9304d8db0f03aba99eb4a8579330535a94268ce
Bledar N <bledar.n@outlook.com>
2026-02-03 22:37:47 -0500

    fix bug

--- celebrity-flashcards-app/src/main.jsx
+++ celebrity-flashcards-app/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode basename="/celebrity-flashcards-app/">
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
 
 // createRoot(document.getElementById('root')).render(
