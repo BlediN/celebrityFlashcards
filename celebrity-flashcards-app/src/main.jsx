4af9bd9ed0b0c62eab988ccdf3499697630f737b
Bledar N <bledar.n@outlook.com>
2026-02-03 22:54:24 -0500

    fix

--- celebrity-flashcards-app/src/main.jsx
+++ celebrity-flashcards-app/src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode basename="/celebrityFlashcards/">
-    <App />
-  </StrictMode>
-)
-
 // createRoot(document.getElementById('root')).render(
-//   <BrowserRouter basename="/celebrityFlashcards/">
+//   <StrictMode basename="/celebrityFlashcards/">
 //     <App />
-//   </BrowserRouter>,
+//   </StrictMode>
 // )
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/celebrityFlashcards/">
+    <App />
+  </BrowserRouter>,
+)
